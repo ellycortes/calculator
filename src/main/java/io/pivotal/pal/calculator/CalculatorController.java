a3b01e485a5d0620996744fcafb0f0db231dc0be
Alejandro Guzman <aguzman@gblsys.com>
2019-04-26 13:57:35 -0700

    calculator app

--- src/main/java/io/pivotal/pal/calculator/CalculatorController.java
+++ src/main/java/io/pivotal/pal/calculator/CalculatorController.java
@@ -3,6 +3,7 @@ package io.pivotal.pal.calculator;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +12,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("calculator")
 public class CalculatorController {
 
-    @GetMapping("add")
+    @PostMapping("add")
     public ResponseEntity add(@RequestBody Calculator calculator) {
         int result = calculator.add();
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @GetMapping("sub")
+    @PostMapping("sub")
     public ResponseEntity sub(@RequestBody Calculator calculator) {
         int result = calculator.sub();
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @GetMapping("mul")
+    @PostMapping("mul")
     public ResponseEntity mul(@RequestBody Calculator calculator) {
         int result = calculator.mul();
         return new ResponseEntity(result, HttpStatus.OK);
     }
 
-    @GetMapping("div")
+    @PostMapping("div")
     public ResponseEntity div(@RequestBody Calculator calculator) {
         int result = calculator.div();
         return new ResponseEntity(result, HttpStatus.OK);
